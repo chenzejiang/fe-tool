424283dffa69b2f75bb59225e9ca05839e8a8576
chenzejiang <820289461@qq.com>
2020-10-15 17:57:19 +0800

    修改引入

--- src/components/content/tool/creat_img.vue
+++ src/components/content/tool/creat_img.vue
@@ -2,13 +2,11 @@
   <div class="tool_box">
     <h1>{{ this.$route.meta.title }}</h1>
     <div id="container">
-      <a
-        id="selectBtn"
-        class="myButton"
-        href="javascript:void(0);"
+      <el-button
         onclick="document.getElementById('input_file').click();"
-      >选择图片</a>
-      <!-- <a id="saveBtn" href="javascript:void(0);" onclick="saveImage();">保存</a> -->
+        type="primary"
+      >选择图片</el-button>
+
       <input
         type="file"
         id="input_file"
@@ -25,6 +23,9 @@
             <strong>{{ picHeight }}</strong>px size：
             <strong>{{ picSize }}</strong>kb
           </p>
+          <div class="">
+
+          </div>
           <canvas id="cropper"></canvas>
 
           <p class="p_btn" style="display:none;">
@@ -82,14 +83,9 @@
         <!-- 放缩略图的元素  -->
         <div id="thumbnail" class="thumbnail">
           <div class="thumbnail_box">
-            <!-- <p>180*180</p> -->
-            <p>
-              <a
-                :href="small_base64"
-                :download="small_pic_name"
-                class="download"
-              >Download</a>
-            </p>
+            <el-button type="success">
+              <a :href="small_base64" :download="small_pic_name">Download</a>
+            </el-button>
             <canvas
               id="smallPic"
               :width="small_pic_width"
@@ -107,6 +103,8 @@
 </template>
 
 <script>
+import '../../../assets/js/createImg';
+
 export default {
   data() {
     return {
@@ -124,335 +122,6 @@ export default {
   // 加载生成图片需要的方法插件
   beforeCreate: function() {
     /* 多尺寸 zc*/
-    (function(i) {
-      var c = function(a, b, d, e) {
-        this.width = a;
-        this.height = b;
-        // 这里是关键
-        this.cropWidth = d;
-        this.cropHeight = e;
-        // this.cropWidth = d;
-        // this.cropHeight = e;
-        this.imageContext = this.imageCanvas = this.image = null;
-        this.imageScale = 1;
-        this.imageViewTop = this.imageViewLeft = this.imageTop = this.imageLeft = this.imageRotation = 0;
-        this.context = this.canvas = null;
-        this.previews = [];
-        this.maskGroup = [];
-        this.maskAlpha = 0.4;
-        this.maskColor = '#fff';
-        this.cropTop = this.cropLeft = 0;
-        this.cropViewWidth = d;
-        this.cropViewHeight = e;
-        this.dragSize = 7;
-        this.dragColor = '#fff';
-        this.mouseY = this.mouseX = this.dragTop = this.dragLeft = 0;
-        this.isResizing = this.isMoving = this.inDragger = this.inCropper = false;
-        this.cropStartHeight = this.cropStartWidth = this.cropStartTop = this.cropStartLeft = this.mouseStartY = this.mouseStartX = 0;
-      };
-      i.ImageCropper = c;
-      c.prototype.setCanvas = function(a) {
-        this.canvas = document.getElementById(a) || a;
-        this.context = this.canvas.getContext('2d');
-        this.canvas.width = this.width;
-        this.canvas.height = this.height;
-        this.canvas.oncontextmenu = this.canvas.onselectstart = function() {
-          return false;
-        };
-        this.imageCanvas = document.createElement('canvas');
-        this.imageContext = this.imageCanvas.getContext('2d');
-        this.imageCanvas.width = this.width;
-        this.imageCanvas.height = this.height;
-      };
-      c.prototype.addPreview = function(a) {
-        this.previews.push((document.getElementById(a) || a).getContext('2d'));
-      };
-      c.prototype.loadImage = function(a) {
-        if (this.isAvaiable() && this.isImage(a)) {
-          var b = new FileReader();
-          var d = this;
-          b.readAsDataURL(a);
-          b.onload = function(e) {
-            if (!d.image) d.image = new Image();
-            d.image.onload = function() {
-              d._init();
-            };
-            d.image.src = e.target.result;
-          };
-        }
-      };
-      c.prototype._init = function() {
-        var a = Math.min(
-          this.width / this.image.width,
-          this.height / this.image.height
-        );
-        if (a > 1) {
-          a = Math.min(
-            this.cropViewWidth / this.image.width,
-            this.cropViewHeight / this.image.height
-          );
-        }
-        if (this.image.width * a < this.cropViewWidth) { a = Math.min(a, this.cropViewWidth / this.image.width); }
-        if (this.image.height * a < this.cropViewHeight) { a = Math.min(a, this.cropViewHeight / this.image.height); }
-        this.imageViewLeft = this.imageLeft =
-          (this.width - this.image.width * a) / 2;
-        this.imageViewTop = this.imageTop =
-          (this.height - this.image.height * a) / 2;
-        this.imageScale = a;
-        this.imageRotation = 0;
-        a = Math.min(this.image.width * a, this.image.height * a);
-        // 注释掉部分是裁剪正方形
-        // this.cropViewWidth = Math.min(a, this.cropWidth);
-        // this.cropViewHeight = Math.min(a, this.cropHeight);
-        this.cropViewWidth = this.cropWidth;
-        this.cropViewHeight = this.cropHeight;
-
-        this.cropLeft = (this.width - this.cropViewWidth) / 2;
-        this.cropTop = (this.height - this.cropViewHeight) / 2;
-        this.dragLeft = this.cropLeft + this.cropViewWidth - this.dragSize / 2;
-        this.dragTop = this.cropTop + this.cropViewHeight - this.dragSize / 2;
-        this._update();
-        var b = this;
-        this.canvas.onmousedown = function(d) {
-          b._mouseHandler.call(b, d);
-        };
-        this.canvas.onmouseup = function(d) {
-          b._mouseHandler.call(b, d);
-        };
-        this.canvas.onmousemove = function(d) {
-          b._mouseHandler.call(b, d);
-        };
-      };
-      // 这是移动的函数
-      c.prototype._mouseHandler = function(a) {
-        if (a.type == 'mousemove') {
-          var b = this.canvas.getClientRects()[0];
-          this.mouseX = a.pageX - b.left - $(window).scrollLeft();
-          this.mouseY = a.pageY - b.top - $(window).scrollTop();
-          this._checkMouseBounds();
-          this.canvas.style.cursor =
-            this.inCropper || this.isMoving
-              ? 'move'
-              : this.inDragger || this.isResizing
-                ? 'se-resize'
-                : '';
-          this.isMoving ? this._move() : this.isResizing && this._resize();
-        } else if (a.type == 'mousedown') {
-          this.mouseStartX = this.mouseX;
-          this.mouseStartY = this.mouseY;
-          this.cropStartLeft = this.cropLeft;
-          this.cropStartTop = this.cropTop;
-          this.cropStartWidth = this.cropViewWidth;
-          this.cropStartHeight = this.cropViewHeight;
-          this.inCropper
-            ? (this.isMoving = true)
-            : this.inDragger && (this.isResizing = true);
-        } else if (a.type == 'mouseup') this.isMoving = this.isResizing = false;
-      };
-      c.prototype._checkMouseBounds = function() {
-        this.inCropper =
-          this.mouseX >= this.cropLeft &&
-          this.mouseX <= this.cropLeft + this.cropViewWidth &&
-          this.mouseY >= this.cropTop &&
-          this.mouseY <= this.cropTop + this.cropViewHeight;
-        this.inDragger =
-          this.mouseX >= this.dragLeft &&
-          this.mouseX <= this.dragLeft + this.dragSize &&
-          this.mouseY >= this.dragTop &&
-          this.mouseY <= this.dragTop + this.dragSize;
-        this.inCropper = this.inCropper && !this.inDragger;
-      };
-      c.prototype._move = function() {
-        var a = this.mouseY - this.mouseStartY;
-        this.cropLeft = Math.max(
-          this.imageViewLeft,
-          this.cropStartLeft + (this.mouseX - this.mouseStartX)
-        );
-        this.cropLeft = Math.min(
-          this.cropLeft,
-          this.width - this.imageViewLeft - this.cropViewWidth
-        );
-        this.cropTop = Math.max(this.imageViewTop, this.cropStartTop + a);
-        this.cropTop = Math.min(
-          this.cropTop,
-          this.height - this.imageViewTop - this.cropViewHeight
-        );
-        this.dragLeft = this.cropLeft + this.cropViewWidth - this.dragSize / 2;
-        this.dragTop = this.cropTop + this.cropViewHeight - this.dragSize / 2;
-        this._update();
-      };
-      c.prototype._resize = function() {
-        var a = Math.min(
-          this.mouseX - this.mouseStartX,
-          this.mouseY - this.mouseStartY
-        );
-        var b = Math.max(10, this.cropStartWidth + a);
-        a = Math.max(10, this.cropStartHeight + a);
-        b = Math.min(b, this.width - this.cropStartLeft - this.imageViewLeft);
-        a = Math.min(a, this.height - this.cropStartTop - this.imageViewTop);
-        this.cropViewWidth = this.cropViewHeight = Math.round(Math.min(b, a));
-        // this.cropViewHeight =
-        this.dragLeft = this.cropLeft + this.cropViewWidth - this.dragSize / 2;
-        this.dragTop = this.cropTop + this.cropViewHeight - this.dragSize / 2;
-        this._update();
-      };
-      c.prototype.rotate = function(a) {
-        if (this.image) {
-          this.imageRotation += a;
-          this.imageViewLeft = (a = Math.abs(this.imageRotation % 180) == 90)
-            ? this.imageTop
-            : this.imageLeft;
-          this.imageViewTop = a ? this.imageLeft : this.imageTop;
-          this.cropLeft = (this.width - this.cropViewWidth) / 2;
-          this.cropTop = (this.height - this.cropViewHeight) / 2;
-          this.dragLeft =
-            this.cropLeft + this.cropViewWidth - this.dragSize / 2;
-          this.dragTop = this.cropTop + this.cropViewHeight - this.dragSize / 2;
-          this._update();
-        }
-      };
-      c.prototype._update = function() {
-        this.context.clearRect(0, 0, this.width, this.height);
-        this._drawImage();
-        this._drawMask();
-        this._drawDragger();
-        this._drawPreview();
-      };
-      c.prototype._drawImage = function() {
-        this.imageContext.clearRect(0, 0, this.width, this.height);
-        this.imageContext.save();
-        var a = this.imageRotation % 360;
-        this.imageContext.translate(this.imageViewLeft, this.imageViewTop);
-        this.imageContext.scale(this.imageScale, this.imageScale);
-        this.imageContext.rotate((this.imageRotation * Math.PI) / 180);
-        switch ((360 - a) % 360) {
-          case 90:
-            this.imageContext.drawImage(this.image, -this.image.width, 0);
-            break;
-          case 180:
-            this.imageContext.drawImage(
-              this.image,
-              -this.image.width,
-              -this.image.height
-            );
-            break;
-          case 270:
-            this.imageContext.drawImage(this.image, 0, -this.image.height);
-            break;
-          default:
-            this.imageContext.drawImage(this.image, 0, 0);
-        }
-        this.imageContext.restore();
-        this.context.drawImage(this.imageCanvas, 0, 0);
-      };
-      // 画预览
-      c.prototype._drawPreview = function() {
-        for (var a = 0; a < this.previews.length; a++) {
-          var b = this.previews[a];
-          b.clearRect(0, 0, b.canvas.width, b.canvas.height);
-          b.save();
-          b.drawImage(
-            this.imageCanvas,
-            this.cropLeft,
-            this.cropTop,
-            this.cropViewWidth,
-            this.cropViewHeight,
-            0,
-            0,
-            b.canvas.width,
-            b.canvas.height
-          );
-          b.restore();
-        }
-      };
-      // 画遮罩层的样式
-      c.prototype._drawMask = function() {
-        this._drawRect(
-          this.imageViewLeft,
-          this.imageViewTop,
-          this.cropLeft - this.imageViewLeft,
-          this.height,
-          this.maskColor,
-          null,
-          this.maskAlpha
-        );
-        this._drawRect(
-          this.cropLeft + this.cropViewWidth,
-          this.imageViewTop,
-          this.width - this.cropViewWidth - this.cropLeft + this.imageViewLeft,
-          this.height,
-          this.maskColor,
-          null,
-          this.maskAlpha
-        );
-        this._drawRect(
-          this.cropLeft,
-          this.imageViewTop,
-          this.cropViewWidth,
-          this.cropTop - this.imageViewTop,
-          this.maskColor,
-          null,
-          this.maskAlpha
-        );
-        this._drawRect(
-          this.cropLeft,
-          this.cropTop + this.cropViewHeight,
-          this.cropViewWidth,
-          this.height - this.cropViewHeight - this.cropTop + this.imageViewTop,
-          this.maskColor,
-          null,
-          this.maskAlpha
-        );
-      };
-      // 画滑块
-      c.prototype._drawDragger = function() {
-        this._drawRect(
-          this.dragLeft,
-          this.dragTop,
-          this.dragSize,
-          this.dragSize,
-          null,
-          this.dragColor,
-          null
-        );
-      };
-      // 画物体的方法
-      c.prototype._drawRect = function(a, b, d, e, f, g, h) {
-        this.context.save();
-        if (f !== null) this.context.fillStyle = f;
-        if (g !== null) this.context.strokeStyle = g;
-        if (h !== null) this.context.globalAlpha = h;
-        this.context.beginPath();
-        this.context.rect(a, b, d, e);
-        this.context.closePath();
-        f !== null && this.context.fill();
-        g !== null && this.context.stroke();
-        this.context.restore();
-      };
-      c.prototype.getCroppedImageData = function(a, b, d) {
-        var e = document.createElement('canvas');
-        e.width = a || this.cropWidth;
-        e.height = b || this.cropHeight;
-        e.getContext('2d').drawImage(
-          this.imageCanvas,
-          this.cropLeft,
-          this.cropTop,
-          this.cropViewWidth,
-          this.cropViewHeight,
-          0,
-          0,
-          e.width,
-          e.height
-        );
-        return e.toDataURL(d || 'image/jpeg');
-      };
-      c.prototype.isAvaiable = function() {
-        return typeof FileReader !== 'undefined';
-      };
-      c.prototype.isImage = function(a) {
-        return /image/i.test(a.type);
-      };
-    })(window);
   },
   // 成功后
   mounted: function() {
@@ -470,7 +139,7 @@ export default {
     // change触发
     selectImage: function(fileList) {
       if (fileList === 'input_file') {
-        var fileList = document.getElementById('input_file');
+        fileList = document.getElementById('input_file');
       }
       if (fileList['files'].length === 0) {
         return false;
@@ -503,6 +172,7 @@ export default {
           that.picWidth = img_width;
           that.picHeight = img_height;
           // 设置画布的大小  (画布的width, 画布的height, 裁剪的方块的width，裁剪的方块的height)
+          // eslint-disable-next-line no-undef
           that.cropper = new ImageCropper(
             img_width,
             img_height,
@@ -523,10 +193,10 @@ export default {
             // 获取缩略图的 canvas
             var smallPic = document.getElementById('smallPic');
             that.small_base64 = convertCanvasToImage(smallPic).src;
+            console.log('生成下载');
           }, 100);
         };
       };
-      // that.$emit('aaaaaa',that.aaa);
     },
     // 旋转图片
     rotateImage: function(e) {
@@ -657,109 +327,12 @@ export default {
     background: #51a3dd;
     border: none;
   }
-  .myButton {
-    margin: 20px 0;
-    -moz-box-shadow: 0px 10px 14px -7px #276873;
-    -webkit-box-shadow: 0px 10px 14px -7px #276873;
-    box-shadow: 0px 10px 14px -7px #276873;
-    background: -webkit-gradient(
-      linear,
-      left top,
-      left bottom,
-      color-stop(0.05, #599bb3),
-      color-stop(1, #408c99)
-    );
-    background: -moz-linear-gradient(top, #599bb3 5%, #408c99 100%);
-    background: -webkit-linear-gradient(top, #599bb3 5%, #408c99 100%);
-    background: -o-linear-gradient(top, #599bb3 5%, #408c99 100%);
-    background: -ms-linear-gradient(top, #599bb3 5%, #408c99 100%);
-    background: linear-gradient(to bottom, #599bb3 5%, #408c99 100%);
-    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#599bb3', endColorstr='#408c99',GradientType=0);
-    background-color: #599bb3;
-    -moz-border-radius: 8px;
-    -webkit-border-radius: 8px;
-    border-radius: 8px;
-    display: inline-block;
-    cursor: pointer;
-    color: #ffffff;
-    font-size: 18px;
-    padding: 13px 50px;
-    text-decoration: none;
-    text-shadow: 0px 1px 0px #3d768a;
-  }
-  .myButton:hover {
-    background: -webkit-gradient(
-      linear,
-      left top,
-      left bottom,
-      color-stop(0.05, #408c99),
-      color-stop(1, #599bb3)
-    );
-    background: -moz-linear-gradient(top, #408c99 5%, #599bb3 100%);
-    background: -webkit-linear-gradient(top, #408c99 5%, #599bb3 100%);
-    background: -o-linear-gradient(top, #408c99 5%, #599bb3 100%);
-    background: -ms-linear-gradient(top, #408c99 5%, #599bb3 100%);
-    background: linear-gradient(to bottom, #408c99 5%, #599bb3 100%);
-    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#408c99', endColorstr='#599bb3',GradientType=0);
-    background-color: #408c99;
-  }
-  .myButton:active {
-    position: relative;
-    top: 1px;
-  }
 
   #saveBtn {
     width: 67px;
     height: 27px;
   }
   .thumbnail {
-    .download {
-      background: -webkit-gradient(
-        linear,
-        left top,
-        left bottom,
-        color-stop(0.05, #89c403),
-        color-stop(1, #77a809)
-      );
-      background: -moz-linear-gradient(top, #89c403 5%, #77a809 100%);
-      background: -webkit-linear-gradient(top, #89c403 5%, #77a809 100%);
-      background: -o-linear-gradient(top, #89c403 5%, #77a809 100%);
-      background: -ms-linear-gradient(top, #89c403 5%, #77a809 100%);
-      background: linear-gradient(to bottom, #89c403 5%, #77a809 100%);
-      filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#89c403', endColorstr='#77a809',GradientType=0);
-      background-color: #89c403;
-      -moz-border-radius: 6px;
-      -webkit-border-radius: 6px;
-      border-radius: 6px;
-      display: inline-block;
-      cursor: pointer;
-      color: #ffffff;
-      font-family: Trebuchet MS;
-      font-size: 15px;
-      font-weight: bold;
-      padding: 5px 18px;
-      text-decoration: none;
-    }
-    .download:hover {
-      background: -webkit-gradient(
-        linear,
-        left top,
-        left bottom,
-        color-stop(0.05, #77a809),
-        color-stop(1, #89c403)
-      );
-      background: -moz-linear-gradient(top, #77a809 5%, #89c403 100%);
-      background: -webkit-linear-gradient(top, #77a809 5%, #89c403 100%);
-      background: -o-linear-gradient(top, #77a809 5%, #89c403 100%);
-      background: -ms-linear-gradient(top, #77a809 5%, #89c403 100%);
-      background: linear-gradient(to bottom, #77a809 5%, #89c403 100%);
-      filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#77a809', endColorstr='#89c403',GradientType=0);
-      background-color: #77a809;
-    }
-    .download:active {
-      position: relative;
-      top: 1px;
-    }
     p {
       margin: 5px 0;
     }
